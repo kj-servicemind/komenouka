a846b12749d17c7c31d90239e79d4d6135d5409c
kj-servicemind <ueda.kg893@gmail.com>
2026-07-22 00:29:10 +0900

    Update photo-ping.js

--- functions/api/photo-ping.js
+++ functions/api/photo-ping.js
@@ -1,1 +1,14 @@
-
+export async function onRequestGet({ env }) {
+  const body = {
+    ok: true,
+    cloud_name: !!env.CLOUDINARY_CLOUD_NAME,
+    api_key: !!env.CLOUDINARY_API_KEY,
+    api_secret: !!env.CLOUDINARY_API_SECRET,
+    passcode: !!env.PHOTO_PASSCODE,
+    folder: env.PHOTO_FOLDER || 'komenouka'
+  };
+  body.ready = body.cloud_name && body.api_key && body.api_secret;
+  return new Response(JSON.stringify(body), {
+    headers: { 'content-type': 'application/json; charset=utf-8', 'cache-control': 'no-store' }
+  });
+}
